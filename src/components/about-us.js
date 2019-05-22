714cc2f00ca0339e611cacddcd173f3813c0e3db
Nikki Fargus <nikki.fargus@newcrosshealthcare.com>
2019-05-22 17:53:07 +0100

    added fullstop to about us

--- src/components/about-us.js
+++ src/components/about-us.js
@@ -8,8 +8,8 @@ const Test1 = () => (
         <img style={{width: '60%', marginBottom: 20}} alt="Peter" src={peterFargus}/>
       </div>
       <p style={{ color: '#565347', textAlign: 'center'}}>
-      Peter is a Business Psychologist and Human Resources Consultant.  Previously he has worked for British Airways and PA Management Consultants.
-      He is a Fellow of the Chartered Institute of Personnel and Development; a Fellow of the Institute of Consulting, a Fellow of the Higher Education Academy and an Associate Fellow of the British Psychological Society. To view Peter&#39;s LinkedIn <a href="https://www.linkedin.com/in/pfargus/">Click here</a>
+      Peter is a Business Psychologist and Human Resources Consultant. Previously he has worked for British Airways and PA Management Consultants.
+      He is a Fellow of the Chartered Institute of Personnel and Development; a Fellow of the Institute of Consulting, a Fellow of the Higher Education Academy and an Associate Fellow of the British Psychological Society. To view Peter&#39;s LinkedIn <a href="https://www.linkedin.com/in/pfargus/">Click here</a>.
       </p>
     </div>
   <div className="col-sm-12 col-lg-5 mt-4">
@@ -19,7 +19,7 @@ const Test1 = () => (
 
     <p style={{color: '#565347', textAlign: 'center'}}>
       Dee is a Database Analyst. She has worked with Peter on assignments aimed at emphasising ‘employee voice’ through the use of employee opinion surveys.
-      Previously she has worked for London Transport and PA Computers and Telecommunications. To view Dee&#39;s LinkedIn <a href="https://www.linkedin.com/in/dee-fargus-20382b12/">Click here</a>
+      Previously she has worked for London Transport and PA Computers and Telecommunications. To view Dee&#39;s LinkedIn <a href="https://www.linkedin.com/in/dee-fargus-20382b12/">Click here</a>.
     </p>
   </div>
 </div>
