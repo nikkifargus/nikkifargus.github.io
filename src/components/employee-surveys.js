f80e1332b10fb1d6c60e80958c9c4f0b3982189b
Nikki Fargus <nikki.fargus@newcrosshealthcare.com>
2021-03-12 17:46:29 +0000

    updated based on peter comments

--- src/components/employee-surveys.js
+++ src/components/employee-surveys.js
@@ -9,15 +9,16 @@ const Index = () => (
         style={{textAlign: 'center'}}
       >
         <img alt="logo" style={{width: '60%'}} src={logo}/>
-        <p className="text-orange"> SCALEUP-GUIDE ENHANCING YOUR LEADERSHIP </p>
+        <p className="text-orange"> SCALEUP- LEADER GUIDE </p>
+        <p className="text-orange"> ENHANCING YOUR LEADERSHIP </p>
 
       </div>
       <div className="col-lg-5 col-sm-12" style={{ margin: 'auto 0'}}>
         <p> Our approach to enabling you to improve your scaleup leadership involves: </p>
         <p> - Your judgement on the importance, in your organisation, of 12 leadership activities. </p>
-        <p> -  Your judgement on how well you are carrying out those activities. </p>
-        <p> - And provides you with: A report indicating your priorities for improved leadership. </p>
-        <p> - Vist <a href="https://www.scaleup-leader.guide/" >Scaleup-Leader.Guide</a> for more information</p>
+        <p> - Your judgement on how well you are carrying out those activities. </p>
+        <p> - And provides you with a report indicating your priorities for improved leadership. </p>
+        <p> - Visit <a href="https://www.scaleup-leader.guide/" >Scaleup-Leader.Guide</a> for more information</p>
       </div>
     </div>
   </div>
--- src/components/our-people.js
+++ src/components/our-people.js
@@ -10,15 +10,16 @@ const Index = () => (
       >
         <img alt="logo" style={{width: '60%'}} src={logo}/>
         <p className="text-orange"> HUMAN CAPITAL REPORT </p>
+        <p className="text-orange"> PORTRAYING YOUR HUMAN CAPITAL QUALITY </p>
 
       </div>
       <div className="col-lg-5 col-sm-12" style={{ margin: 'auto 0'}}>
         <p> Reviews of workforce quality before scale-up, transformation, merger or aquisition. Annual reviews of workforce strategy </p>
-        <p> We help clients assess the quality of Human Capital: </p>
+        <p> We help clients portray the quality of their Human Capital: </p>
         <p> - During strategic planning </p>
         <p> - Before transformation </p>
         <p> - Before mergers and acquisitions </p>
-        <p> - Vist <a href="http://www.ourpeoplereview.com">ourpeoplereview.com</a> for more information</p>
+        <p> - Visit <a href="http://www.ourpeoplereview.com">ourpeoplereview.com</a> for more information</p>
       </div>
     </div>
   </div>
